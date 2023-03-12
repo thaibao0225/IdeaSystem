9c9ea2435d800c82de4aced0ffa8228e4778812d
thaibao <thaibao0225@gmail.com>
2023-03-12 18:26:49 +0700

    Done Test for new feature

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Hosting;
 using System;
+using System.Data;
+using ClosedXML.Excel;
 
 namespace IdeaSystem.Controllers
 {
@@ -25,77 +27,83 @@ namespace IdeaSystem.Controllers
         [Route("/test")]
         public ActionResult Index()
         {
-            var filepath = Path.Combine(Environment.CurrentDirectory, "UploadedFiles", "New Text Document.txt");
-            return File(System.IO.File.ReadAllBytes(filepath), "text/plain", System.IO.Path.GetFileName(filepath));
-        }
+            //Downfile ----------------------------------------
+            //var filepath = Path.Combine(Environment.CurrentDirectory, "UploadedFiles", "New Text Document.txt");
+            //return File(System.IO.File.ReadAllBytes(filepath), "text/plain", System.IO.Path.GetFileName(filepath));
 
-        // GET: TestController/Details/5
-        public ActionResult Details(int id)
-        {
+
+            //Create Excel file ----------------------------------------
+            //List<string> list = new List<string>();
+            //list.Add("1");
+            //list.Add("2");
+            //list.Add("3");
+            //list.Add("4");
+
+
+            //var testDt = ToDataTable(list);
+            //var resuilt = ToExcelFile(testDt, "testExcel");
+
+
+            //Downfile ----------------------------------------
+            //var filepath = Path.Combine(Environment.CurrentDirectory, "ExcelFile", "testExcel.xlsx");
+            //return File(System.IO.File.ReadAllBytes(filepath), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", System.IO.Path.GetFileName(filepath));
+
+
+
+
+
             return View();
         }
 
-        // GET: TestController/Create
-        public ActionResult Create()
+        public bool ToExcelFile( DataTable dt, string filename)
         {
-            return View();
+            bool Success = false;
+            //try
+            //{
+            XLWorkbook wb = new XLWorkbook();
+
+            wb.Worksheets.Add(dt, "Sheet 1");
+
+            if (filename.Contains("."))
+            {
+                int IndexOfLastFullStop = filename.LastIndexOf('.');
+
+                filename = filename.Substring(0, IndexOfLastFullStop) + ".xlsx";
+
+            }
+            string path = Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, "ExcelFile"));
+            filename = path + "\\" + filename + ".xlsx";
+
+            wb.SaveAs(filename);
+
+            Success = true;
+            return Success;
         }
 
-        // POST: TestController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public DataTable ToDataTable(List<string> list)
         {
-            try
+            DataTable MethodResult = null;
+
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Item");
+            dt.Columns.Add("Item2");
+
+            foreach (string s in list)
             {
-                return RedirectToAction(nameof(Index));
+                DataRow dr = dt.NewRow();
+                dr[0] = s;
+                dr[1] = s;
+                dt.Rows.Add(dr);
+
             }
-            catch
-            {
-                return View();
-            }
+
+            dt.AcceptChanges();
+
+            MethodResult = dt;
+
+            return MethodResult;
+
         }
 
-        // GET: TestController/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: TestController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: TestController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: TestController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
     }
 }
